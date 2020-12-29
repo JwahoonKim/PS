604d739f2bfcf6e8b75524caffd67bcdfca20d2e
JwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2020-12-30 00:53:59 +0900

    2020.12.30

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
-n = int(input())
-i = 1
-number = 1
-arr = [0] * n
-for i in range(1, n + 1):
-    for j in range(i):
-        print(number, end=" ")
-        arr[j] += number
-        number += 1
-    print('')
-print(' '.join(map(str, arr)))
+triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
+
+
+def solution(triangle):
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                triangle[i][j] += triangle[i - 1][0]
+            elif j == len(triangle[i]) - 1:
+                triangle[i][j] += triangle[i - 1][j - 1]
+            else:
+                left = triangle[i - 1][j - 1]
+                right = triangle[i - 1][j]
+                triangle[i][j] += max(left, right)
+
+    print(max(triangle[len(triangle) - 1]))
+    return max(triangle[len(triangle) - 1])
+
+
+solution(triangle)
+print(triangle)
