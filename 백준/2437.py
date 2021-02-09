8c4910888201ffc35c60413dc23c10054f9e10dd
JwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-02-10 01:39:40 +0900

    2021.02.10 PS

--- 백준/2437.py
+++ 백준/2437.py
@@ -1,42 +1,11 @@
-from itertools import combinations as c
-from bisect import bisect_right as bs
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 W = list(map(int, input().split()))
-dp = [False for _ in range(max(W) + 1)]
-for i in W:
-    dp[i] = True
 W.sort()
 answer = 0
-
-if W[0] > 1:
-    answer = W[0] - 1
-else:
-    cur = 2
-    while(1):
-        if dp[cur] == True:
-            cur += 1
-            continue
-        flag = False
-        bsIndex = bs(W, cur)
-        for i in range(bsIndex):
-            temp = 0
-            for j in range(i, bsIndex):
-                temp += W[j]
-                if temp > cur:
-                    break
-                elif temp == cur:
-                    cur += 1
-                    flag = True
-                    break
-            if flag == True:
-                break
-        if flag == False:
-            answer = cur
-            break
-print(answer)        
-
-
-            
+# W의 i행까지의 합 + 1이 W[i + 1]보다 작으면 sum(W[:i + 1]) + 1은 못만들고 이것이 답
+for i in range(n):
+    if answer + 1 < W[i]:
+        break
+    else:
+        answer += W[i]
+print(answer + 1)
