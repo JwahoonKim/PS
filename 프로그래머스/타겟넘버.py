b929c94449a00ba1bb0ff2f85e242669f9c8b53c
JwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-02-14 01:56:16 +0900

    Programmers : 타겟넘버
    Programmers : 타겟넘버 풀이

--- 프로그래머스/타겟넘버.py
+++ 프로그래머스/타겟넘버.py
@@ -1,11 +1,17 @@
-
+from collections import Counter
 
 def solution(numbers, target):
     answer = 0
+    #case는 numbers를 이용하여 만들 수 있는 모든 수
     case = dfs(0, [0], numbers)
-
+    
+    count = Counter(case)
+    answer = count[target]
     return answer
 
+# 1. i단계마다 만들 수 있는 경우의 수를 배열에 담고
+# 2. i + 1 단계에 그 배열을 계속 넘겨주면
+# 3. 마지막 단계에서 만들어진 배열은 numbers를 이용하여 만들수 있는 전체 경우를 담은 배열이다.
 def dfs(i, nowArr, numbers):
     if i == len(numbers):
         return nowArr
