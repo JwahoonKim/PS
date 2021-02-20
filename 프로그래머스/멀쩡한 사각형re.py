e0297938e69a08457514487cbe5e2bf12c14c583
JwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-02-21 01:50:33 +0900

    Programmers: 멀쩡한 사각형(Re)
    이전 버전에 주석 추가

--- 프로그래머스/멀쩡한 사각형re.py
+++ 프로그래머스/멀쩡한 사각형re.py
@@ -4,10 +4,13 @@ def solution(w,h):
     answer = 0
     x = min(w, h)
     y = max(w, h)
+    # 시간 줄이기용
     if x == y:
         return x ** 2 - x
     if x == 1:
         return 0
+    # 직선의 방정식에서 자연수 x에 대한 치역값을 각각 floor해주고
+    # 대칭이므로 * 2 해서 다 더하면 답
     for i in range(1, x):
         answer += 2 * math.floor(y * i / x)
     return answer
