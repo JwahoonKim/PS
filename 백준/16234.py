d396148681603ba2936576af4ac798499ff9968a
JwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-03-07 21:44:33 +0900

    Baekjoon: 16234(인구 이동)

--- 백준/16234.py
+++ 백준/16234.py
@@ -22,42 +22,42 @@ def dfs(x, y, graph):
         return False
     return False
 
-
-# 입력받기
-n, lower, upper = map(int, input().split())
-answer = 0
-stack = []
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
-visited = [[False] * n for _ in range(n)]
-unions = []
-
-while 1:
-    for i in range(n):
-        for j in range(n):
-            if dfs(i, j, graph) == True:
-                # union이 형성되면 연합국들 모임을 unions에 append
-                if len(stack) > 1:
-                    unions.append(stack)
-                stack = []
-    if not unions:
-        break
-    else:
-        answer += 1
-        for union in unions:
-            people = 0
-            # 인구조사
-            for nation in union:
-                x, y = nation
-                people += graph[x][y]
-            # 인구 이동한 후 인구수
-            result = people // len(union)
-            # 인구 이동 적용
-            for nation in union:
-                x, y = nation
-                graph[x][y] = result
-    # reset
-    unions = []
+if __name__ == '__main__':
+    # 입력받기
+    n, lower, upper = map(int, input().split())
+    answer = 0
+    stack = []
+    graph = []
+    for _ in range(n):
+        graph.append(list(map(int, input().split())))
     visited = [[False] * n for _ in range(n)]
-print(answer)
+    unions = []
+
+    while 1:
+        for i in range(n):
+            for j in range(n):
+                if dfs(i, j, graph) == True:
+                    # union이 형성되면 연합국들 모임을 unions에 append
+                    if len(stack) > 1:
+                        unions.append(stack)
+                    stack = []
+        if not unions:
+            break
+        else:
+            answer += 1
+            for union in unions:
+                people = 0
+                # 인구조사
+                for nation in union:
+                    x, y = nation
+                    people += graph[x][y]
+                # 인구 이동한 후 인구수
+                result = people // len(union)
+                # 인구 이동 적용
+                for nation in union:
+                    x, y = nation
+                    graph[x][y] = result
+        # reset
+        unions = []
+        visited = [[False] * n for _ in range(n)]
+    print(answer)
