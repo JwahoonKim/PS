8984fc97fd128e2cb847939d9c6bc8b4efd3a244
JwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-03-10 11:59:15 +0900

    Baekjoon: 18353(병사 배치하)

---
+++ 백준/18353.py
@@ -0,0 +1,8 @@
+n = int(input())
+soldiers = list(map(int, input().split()))
+dp = [1] * n
+for i in range(1, n):
+    for j in range(i, -1, -1):
+        if soldiers[j] > soldiers[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(n - max(dp))
