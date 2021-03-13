493ab75485d5e99172f9587b1ac2ce035180cc3f
JwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-03-13 22:43:24 +0900

    Baekjoon: 1715(카드 정렬하기)

--- 백준/1715.py
+++ 백준/1715.py
@@ -1,5 +1,4 @@
-import sys, heapq
-input = sys.stdin.readline
+import heapq
 
 n = int(input())
 answer = 0
@@ -8,9 +7,12 @@ numbers = []
 for _ in range(n):
     heapq.heappush(numbers, int(input()))
 
-
 while(len(numbers) > 1):
+    # 두개 뽑고 비교 
     compare = heapq.heappop(numbers) + heapq.heappop(numbers)
+    # 비교한 양 누적시키기
     answer += compare
+    # 합친 숫자 묶음 다시 힙큐에 삽입
     heapq.heappush(numbers, compare)
+    
 print(answer)
