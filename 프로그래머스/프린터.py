b79053df3bb52ccce10f9afd701b1f7886b11ef8
JwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-03-17 01:13:34 +0900

    Programmers: 프린터

--- 프로그래머스/프린터.py
+++ 프로그래머스/프린터.py
@@ -1,30 +1,22 @@
 from collections import deque
-import heapq
 
 
-def solution(priorities, location):
-    answer = 0
+def solution(priorities, target):
     cur = 0
     count = 0
-    printer = deque() 
+    printer = deque()
     for i in range(len(priorities)):
         printer.append([priorities[i], i])
     priorities.sort(reverse=True)
-    
-    while(1):
-        now = priorities[cur]
-        prior, locate = printer.popleft()
-        
-        if prior != now:
-            printer.append([prior, locate])
-        elif prior == now:
+
+    while 1:
+        highestPrior = priorities[cur]
+        prior, location = printer.popleft()
+        # 더 높은 우선순위가 존재하면 뒤에 다시 붙여
+        if prior != highestPrior:
+            printer.append([prior, location])
+        elif prior == highestPrior:
             cur += 1
             count += 1
-            if locate == location:
+            if location == target:
                 return count
-    return answer
-
-
-
-print(solution([2, 1, 3, 2], 2))
-
