34b54e54eec24648376243f1c7eba9116a38dba7
JwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-03-24 14:56:59 +0900

    BOJ_2609(최대공약수와 최소공배수)

--- 백준/2609.py
+++ 백준/2609.py
@@ -1,13 +1,12 @@
 a, b = map(int, input().split())
 div = min(a, b)
-G = 1
-L = a * b
-while(div != 1):
+
+while 1:
     if a % div == 0 and b % div == 0:
-        G *= div
-        L = G * (a // div) * (b // div)
         break
-    else: 
-        div -= 1
+    div -= 1
+
+G = div
+L = G * (a // div) * (b // div)
 print(G)
 print(L)
