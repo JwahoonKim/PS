ce6329abfacf45445142cfd5fde9ed2b0c67e7af
JwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-04-04 00:17:58 +0900

    BOJ_10610(30)

--- 백준/10610.py
+++ 백준/10610.py
@@ -1,22 +1,7 @@
-list = sorted(list(map(int, input())))
+numList = sorted(list(map(int, input())), reverse=True)
+
 # 30의 배수를 만들지 못하는 조건
-print("123".split(''))
-if 0 not in list or sum(list) % 3 != 0:
+if 0 not in numList or sum(numList) % 3 != 0:
     print(-1)
 else:
-    length = len(list)
-    # 주어진 길이로 만들 수 있는 30의 배수 중 최대 값
-    # ex.) len --> 4  --> 9990
-    maxNumber = ""
-    for _ in range(length - 1):
-        maxNumber += '9'
-    maxNumber = (maxNumber + '0')
-    number = maxNumber
-
-    while(int(number) >= 10 ** (length - 1)):
-        numList = sorted(list(number))
-        if numList == list:
-            break
-        number = str(int(number) - 30)
-
-    print(number)
+    print(int(''.join(map(str, numList))))
