349d7f49eae1c1bb556488182ed45266f5e2e430
jwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-06-04 13:55:37 +0900

    Programmers: 완주하지못한선

--- 프로그래머스/완주하지못한선수.py
+++ 프로그래머스/완주하지못한선수.py
@@ -1,12 +1,9 @@
 def solution(participant, completion):
-    part = sorted(participant)
-    comp = sorted(completion)
-    ans = ""
-    for i in range(len(comp)):
-        if part[i] != comp[i]:
-            if part[i + 1] == comp[i]:
-                ans = part[i]
-                break         
-    if ans == "":
-        ans = part[-1]
-    return ans
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i] == completion[i]:
+            continue
+        else:
+            return participant[i]
+    return participant[len(participant) - 1]
