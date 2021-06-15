2a0c0d76b4d19ba51374ef17c0f149294d205f33
jwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-06-15 17:35:58 +0900

    Programmers: 프린터(풀이추가)

--- 프로그래머스/프린터.py
+++ 프로그래머스/프린터.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-
+# 첫 풀이
 def solution(priorities, target):
     cur = 0
     count = 0
@@ -20,3 +20,20 @@ def solution(priorities, target):
             count += 1
             if location == target:
                 return count
+
+# 두 번째 풀이
+def solution(priorities, target):
+    prior_index = deque(list(enumerate(priorities)))
+    prior_sorted = deque(sorted(priorities, reverse=True))
+    count = 1
+    while(prior_sorted):
+        # 우선순위가 더 높은게 있다면
+        if prior_index[0][1] != prior_sorted[0]:
+            prior_index.append(prior_index.popleft())
+        else:
+            prior_sorted.popleft()
+            if prior_index[0][0] == target:
+                return count
+            else:
+                count += 1
+                prior_index.popleft()
