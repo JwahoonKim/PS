dc4e902913c4e7eb0db0b808551e26e64a5c57fc
jwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-06-16 17:54:17 +0900

    Programmers: 더 맵게(수정)

--- 프로그래머스/더 맵게.py
+++ 프로그래머스/더 맵게.py
@@ -1,5 +1,8 @@
 import heapq
 
+# 첫 풀이
+
+
 def solution(scoville, K):
     count = 0
     heapq.heapify(scoville)
@@ -13,3 +16,17 @@ def solution(scoville, K):
             count += 1
         else:
             return count
+
+# 두 번째 풀이
+
+
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    answer = 0
+    while(scoville[0] < K):
+        if len(scoville) == 1:
+            return -1
+        answer += 1
+        mix = heapq.heappop(scoville) + 2 * heapq.heappop(scoville)
+        heapq.heappush(scoville, mix)
+    return answer
