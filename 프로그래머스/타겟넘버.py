ef3bf96a94ad323253bf93247c8bf3f07f22bc08
jwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-06-18 18:47:03 +0900

    Programmers: 타겟넘버

--- 프로그래머스/타겟넘버.py
+++ 프로그래머스/타겟넘버.py
@@ -1,10 +1,13 @@
 from collections import Counter
 
+# 첫 풀이
+
+
 def solution(numbers, target):
     answer = 0
-    #case는 numbers를 이용하여 만들 수 있는 모든 수
+    # case는 numbers를 이용하여 만들 수 있는 모든 수
     case = dfs(0, [0], numbers)
-    
+
     count = Counter(case)
     answer = count[target]
     return answer
@@ -12,6 +15,8 @@ def solution(numbers, target):
 # 1. i단계마다 만들 수 있는 경우의 수를 배열에 담고
 # 2. i + 1 단계에 그 배열을 계속 넘겨주면
 # 3. 마지막 단계에서 만들어진 배열은 numbers를 이용하여 만들수 있는 전체 경우를 담은 배열이다.
+
+
 def dfs(i, nowArr, numbers):
     if i == len(numbers):
         return nowArr
@@ -22,7 +27,28 @@ def dfs(i, nowArr, numbers):
         nextArr.append(num - curNumber)
     return dfs(i + 1, nextArr, numbers)
 
-numbers = [1,1,1,1,1]
-target = 3
-print(dfs(0,[0], numbers))
 
+numbers = [1, 1, 1, 1, 1]
+target = 3
+print(dfs(0, [0], numbers))
+
+
+# 두 번째 풀이
+
+answer = 0
+
+
+def solution(numbers, target):
+    dfs(numbers, len(numbers), 0, 0, target)
+    global answer
+    return answer
+
+
+def dfs(numbers, length, i, value, target):
+    if i == length:
+        if value == target:
+            global answer
+            answer += 1
+    else:
+        dfs(numbers, length, i + 1, value + numbers[i], target)
+        dfs(numbers, length, i + 1, value - numbers[i], target)
