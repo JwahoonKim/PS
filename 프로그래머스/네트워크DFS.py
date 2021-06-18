a4165ff80b981b433cf18127263fcd9d71b14b41
jwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-06-18 19:03:35 +0900

    Programmers: 네트워크 DFS 추가

--- 프로그래머스/네트워크DFS.py
+++ 프로그래머스/네트워크DFS.py
@@ -1,3 +1,4 @@
+# 첫 풀이
 def dfs(start, visited, computers, n):
     if visited[start] == False:
         visited[start] = True
@@ -16,3 +17,27 @@ def solution(n, computers):
             answer += 1
     return answer
 
+# 두 번째 풀이
+
+
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+    for i in range(n):
+        if dfs(n, computers, visited, i) == True:
+            answer += 1
+    return answer
+
+
+def dfs(n, computers, visited, i):
+    if visited[i] == True:
+        return False
+    else:
+        for j in range(n):
+            if i == j:
+                continue
+            else:
+                if computers[i][j] == 1 and visited[j] == False:
+                    dfs(n, computers, visited, j)
+                    visited[j] = True
+        return True
