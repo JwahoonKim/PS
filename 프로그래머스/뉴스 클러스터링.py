d9d7a44cd609dd91a2aaf137673c02d7d235f67e
jwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-06-29 17:34:34 +0900

    Programmers: 뉴스 클러스터링

---
+++ 프로그래머스/뉴스 클러스터링.py
@@ -0,0 +1,14 @@
+def solution(str1, str2):
+    str1 = [str1[i:i+2].lower() for i in range(len(str1)-1) if str1[i:i+2].isalpha() ]
+    str2 = [str2[i:i+2].lower() for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
+
+    gyo = set(str1) & set(str2)
+    hap = set(str1) | set(str2)
+
+    if len(hap) == 0:
+        return 65536
+
+    gyo_sum = sum([min(str1.count(gg), str2.count(gg)) for gg in gyo])
+    hap_sum = sum([max(str1.count(hh), str2.count(hh)) for hh in hap])
+
+    return int((gyo_sum/hap_sum)*65536)
