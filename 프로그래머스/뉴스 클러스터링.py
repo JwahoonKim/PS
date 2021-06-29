ac9ea49b6f4b5fa06abc2267aa92dbc194116e38
jwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-06-29 17:35:34 +0900

    Programmers: 뉴스 클러스터링

--- 프로그래머스/뉴스 클러스터링.py
+++ 프로그래머스/뉴스 클러스터링.py
@@ -1,6 +1,8 @@
 def solution(str1, str2):
-    str1 = [str1[i:i+2].lower() for i in range(len(str1)-1) if str1[i:i+2].isalpha() ]
-    str2 = [str2[i:i+2].lower() for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
+    str1 = [str1[i:i+2].lower()
+            for i in range(len(str1)-1) if str1[i:i+2].isalpha()]
+    str2 = [str2[i:i+2].lower()
+            for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
 
     gyo = set(str1) & set(str2)
     hap = set(str1) | set(str2)
