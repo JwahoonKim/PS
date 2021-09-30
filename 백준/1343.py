2d205c379e9a84a008aab03e1067c4f931e2feff
jwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-09-30 17:49:48 +0900

    Baekjoon: 1343(폴리오미노)

--- 백준/1343.py
+++ 백준/1343.py
@@ -1,38 +1,6 @@
-def makeString(type, count):
-    if type == "X":
-        if count % 2 != 0:
-            return False
-        return "AAAA" * (count // 4) + ("BB" if count % 4 != 0 else "")
-    else:
-        return "." * count
-
-
 poly = input()
-answer = ""
-type = "X" if poly[0] == "X" else "."
-cursor = 0
-fail = False
 
-while(cursor < len(poly)):
-    count = 0
-    while(cursor < len(poly)):
-        if type == poly[cursor]:
-            count += 1
-            cursor += 1
-        else:
-            if makeString(type, count):
-                answer += makeString(type, count)
-            else:
-                fail = True
-            type = "." if poly[cursor] == "." else "X"
-            break
+poly = poly.replace("XXXX", "AAAA")
+poly = poly.replace("XX", "BB")
 
-if makeString(type, count):
-    answer += makeString(type, count)
-else:
-    fail = True
-
-if not fail:
-    print(answer)
-else:
-    print(-1)
+print(-1) if 'X' in poly else print(poly)
