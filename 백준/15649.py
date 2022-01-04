34b490710735abe534cd2ef9efcbb13931801bb1
jwahoonkim <kjhoon0330@snu.ac.kr>
2022-01-05 01:43:21 +0900

    [BOJ] 15649(N과 M(1))

--- 백준/15649.py
+++ 백준/15649.py
@@ -1,6 +1,8 @@
-from itertools import combinations
+from itertools import permutations
+
 n, m = map(int, input().split())
-arr = list(combinations(range(1, n + 1), m))
+arr = list(permutations(range(1, n + 1), m))
+
 for i in arr:
     for j in range(m):
         print(i[j], end = ' ')
