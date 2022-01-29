8d48df17771ffc5a397fdc42368d41951f6d5c17
jwahoonkim <kjhoon0330@snu.ac.kr>
2022-01-29 22:47:43 +0900

    [Programmers] 보석 쇼핑

---
+++ 프로그래머스/보석 쇼핑.py
@@ -0,0 +1,34 @@
+def dictAdd(dict, value):
+    if value in dict:
+        dict[value] += 1
+    else:
+        dict[value] = 1
+        
+def setStartPointer(start, dict, gems):
+    while 1:
+        if dict[gems[start]] > 1:
+            dict[gems[start]] -= 1
+            start += 1
+        else:
+            return start   
+
+def solution(gems):
+    kinds = len(set(gems))
+    length = len(gems)
+    answer = [0, 1000002]
+    dic = dict()
+    start, end = 0, 0
+    dictAdd(dic, gems[0])
+    
+    while 1:
+        start = setStartPointer(start, dic, gems)
+        if len(dic) == kinds and (end - start) < (answer[1] - answer[0]):
+            answer = [start + 1, end + 1]
+            if end - start == kinds - 1:
+                return answer
+        end += 1
+        if end == length:
+            return answer
+        dictAdd(dic, gems[end])
+        
+    return [start + 1, end + 1]
