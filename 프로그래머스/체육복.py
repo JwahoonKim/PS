934e3aac0cde68220d971d2bbac1e132d2d38c55
jwahoonKim <kjhoon0330@snu.ac.kr>
2022-03-08 12:38:59 +0900

    [Programmers] 체육복

--- 프로그래머스/체육복.py
+++ 프로그래머스/체육복.py
@@ -1,16 +1,13 @@
-import copy 
-
 def solution(n, lost, reserve):
-    arr = copy.deepcopy(reserve)
-    for i in reserve:
-        if i in lost:
-            lost.remove(i)
-            arr.remove(i)
-    for j in arr:
-        if j in lost:
-            lost.remove(j)
-        elif j - 1 in lost:
-            lost.remove(j - 1)
-        elif j + 1 in lost:
-            lost.remove(j + 1)
-    return n - len(lost)
+    # 도난 + 여벌 학생 지우기
+    _lost = sorted([i for i in lost if i not in reserve])
+    _reserve = sorted([i for i in reserve if i not in lost])
+    
+    # 여벌 체육복 빌려주기
+    for r in _reserve:
+        if r - 1 in _lost:
+            _lost.remove(r - 1)
+        elif r + 1 in _lost:
+            _lost.remove(r + 1)
+            
+    return n - len(_lost)
