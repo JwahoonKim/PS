8f47cebcb7f90456c273993f52e7394de8a44d7e
jwahoonKim <kjhoon0330@snu.ac.kr>
2022-05-31 23:08:28 +0900

    BOJ: 1157(단어공부) [Java]

--- 백준/Java/src/BOJ_10798.java
+++ 백준/Java/src/BOJ_10798.java
@@ -1,10 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class BOJ_10798 {
 
