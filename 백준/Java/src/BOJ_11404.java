e62627afdc3f34753b951b5eb71102303e14f029
jwahoonKim <kjhoon0330@snu.ac.kr>
2022-07-26 18:27:35 +0900

    BOJ: 11404(플로이드) - Java

---
+++ 백준/Java/src/BOJ_11404.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_11404 {
+    static int n, m;
+    static final int INF = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        long[][] dist = new long[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(dist[i], INF);
+        }
+        for (int i = 1; i <= n; i++) {
+            dist[i][i] = 0;
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            dist[start][end] = Math.min(dist[start][end], cost);
+        }
+
+        // 플로이드 워셜
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                sb.append(dist[i][j] == INF ? 0 : dist[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
