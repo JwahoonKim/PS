d3dd8124bbd1c409ebd55af674435f1624dc1f6b
jwahoonkim <kjhoon0330@snu.ac.kr>
2023-05-14 00:23:11 +0900

    LeetCode: 13. Roman to Integer

---
+++ LeetCode/Python/13_Roman to Integer.py
@@ -0,0 +1,27 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+
+        s = s.replace("IV", "IIII")\
+            .replace("IX", "VIIII")\
+            .replace("XL", "XXXX")\
+            .replace("XC", "LXXXX")\
+            .replace("CD", "CCCC")\
+            .replace("CM", "DCCCC")
+
+        answer = 0
+        for c in s:
+            answer += roman_map[c]
+
+        return answer
+
+
+print(Solution().romanToInt("III"))
