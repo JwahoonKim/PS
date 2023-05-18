ecee0710f415b22683bae2bc9712a469894029f8
jwahoonkim <kjhoon0330@snu.ac.kr>
2023-05-19 00:02:12 +0900

    BOJ: 2205. 벽 부수고 이동하기

--- 백준/Python/2206.py
+++ 백준/Python/2206.py
@@ -1,39 +1,47 @@
 from collections import deque
 
+INF = int(1e9)
+dr = [1, -1, 0, 0]
+dc = [0, 0, 1, -1]
+
 n, m = map(int, input().split())
+
 graph = []
 for _ in range(n):
     graph.append(list(map(int, input())))
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-dist_without_crash = [[-1] * m for _ in range(n)]
-dist_with_crash = [[-1] * m for _ in range(n)]
-dist_without_crash[0][0] = 1
-dist_with_crash[0][0] = 1
+distance = [[[0] * 2 for _ in range(m)] for _ in range(n)]
+distance[0][0][0] = 1
 
 q = deque()
-q.append((0, 0, False)) # (x, y, 벽 뚫어본 여부)
-
+q.append((0, 0, 0))
 while q:
-    x, y, crashed = q.popleft()
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m and (nx, ny) != (0, 0):
-            if graph[nx][ny] == 1 and not crashed:
-                dist_with_crash[nx][ny] = dist_without_crash[x][y] + 1
-                q.append((nx, ny, True))
-            elif graph[nx][ny] == 0 and not crashed and dist_without_crash[nx][ny] == -1:
-                dist_without_crash[nx][ny] = dist_without_crash[x][y] + 1
-                q.append((nx, ny, False))
-            elif graph[nx][ny] == 0 and crashed and dist_with_crash[nx][ny] == -1:
-                dist_with_crash[nx][ny] = dist_with_crash[x][y] + 1
-                q.append((nx, ny, True))
+    r, c, crushed = q.popleft()
 
-if dist_with_crash[-1][-1] == -1 and dist_without_crash[-1][-1] == -1:
+    if r == n - 1 and c == m - 1:
+        break
+
+    for i in range(4):
+        nr = r + dr[i]
+        nc = c + dc[i]
+
+        if not (0 <= nr < n and 0 <= nc < m):
+            continue
+
+        if graph[nr][nc] == 1 and not crushed and distance[nr][nc][1] == 0:
+            distance[nr][nc][1] = distance[r][c][crushed] + 1
+            q.append((nr, nc, 1))
+
+        elif graph[nr][nc] == 0 and distance[nr][nc][crushed] == 0:
+            distance[nr][nc][crushed] = distance[r][c][crushed] + 1
+            q.append((nr, nc, crushed))
+
+
+if distance[n - 1][m - 1][0] == 0 and distance[n - 1][m - 1][1] == 0:
     print(-1)
-elif dist_with_crash[-1][-1] != -1 and dist_without_crash[-1][-1] != -1:
-    print(min(dist_without_crash[-1][-1], dist_with_crash[-1][-1]))
+elif distance[n - 1][m - 1][0] == 0:
+    print(distance[n - 1][m - 1][1])
+elif distance[n - 1][m - 1][1] == 0:
+    print(distance[n - 1][m - 1][0])
 else:
-    print(max(dist_without_crash[-1][-1], dist_with_crash[-1][-1]))
+    print(min(distance[n - 1][m - 1][0], distance[n - 1][m - 1][1]))
